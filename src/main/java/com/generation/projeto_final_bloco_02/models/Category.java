610cdccf0b350877b8b7a1cf7686722ac95f0ace
GiulioArantes <giulio.arantes@icloud.com>
2025-05-05 11:15:58 -0300

    Segunda Etapa — Construção do primeiro CRUD sem Relacionamento

---
+++ src/main/java/com/generation/projeto_final_bloco_02/models/Category.java
@@ -0,0 +1,38 @@
+package com.generation.projeto_final_bloco_02.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String department;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
